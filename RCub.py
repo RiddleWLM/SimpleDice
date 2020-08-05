228925e68e840155cfd59ccfc3117a0cb6635112
Riddle <hykak0@mail.ru>
2020-08-06 01:27:34 +0500

    Finaly

--- RCub.py
+++ RCub.py
@@ -8,22 +8,12 @@ x = round(random.uniform(1, 6))
 
 print(x)
 
-if x==1:
-    img=PhotoImage(file='cub1.png')
-if x==2:
-    img=PhotoImage(file='cub2.png')
-if x==3:
-    img=PhotoImage(file='cub3.png')
-if x==4:
-    img=PhotoImage(file='cub4.png')
-if x==5:
-    img=PhotoImage(file='cub5.png')
-if x==6:
-    img=PhotoImage(file='cub6.png')
+try:
+    img=PhotoImage(file='cub' + str(x) + '.png')
+except:
+    print('Not found' + ' ' + x)
 
-l = Label(root, image=img)
-
+l = Label(image=img)
 l.pack()
 
-root.geometry()
 root.mainloop()
